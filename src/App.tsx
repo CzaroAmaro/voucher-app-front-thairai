1357a1cb8b15fc6f5e08d315adac08954612b558
Cezary Babik <babik.czarek@gmail.com>
2025-02-12 13:09:15 +0100

    dark-mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import VoucherList from "./components/voucherList/VoucherList.tsx";
 import Navbar from "./components/navbar/Navbar.tsx";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './App.css'
 
 const App: React.FC = () => {
+    const [darkMode, setDarkMode] = useState(
+        localStorage.getItem('theme') === 'dark'
+    );
+    const toggleDarkMode = () => {
+        setDarkMode((prevMode) => !prevMode);
+    };
+
+    useEffect(() => {
+        const theme=darkMode? "dark" : "light";
+        localStorage.setItem("theme", theme);
+    },[darkMode]);
     return (
         <BrowserRouter>
-            <div className="app-container" dark-theme="dark">
+            <div className="app-container" data-theme={darkMode? "dark" : "light"}>
                 <aside className="sidebar">
-                    <Navbar />
+                    <Navbar toggleDarkMode={toggleDarkMode} darkMode={darkMode} />
                 </aside>
-                <main className="main-content">
+                <main className="main-content" dark-theme="dark">
                     <Routes>
                         <Route path="/" element={<VoucherList />} />
                     </Routes>
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,17 +2,30 @@ import React, {useEffect, useState} from 'react';
 import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
-const Navbar: React.FC = () => {
+interface NavbarProps {
+    toggleDarkMode: () => void;
+    darkMode: boolean;
+}
+
+const Navbar: React.FC<NavbarProps> = ({toggleDarkMode, darkMode}) => {
     return (
         <nav className="navbar">
             <h1>Voucher App</h1>
             <ul>
                 <li>
-                    <NavLink to="/" className={({ isActive }) => isActive ? 'active' : ''}>
+                    <NavLink to="/" className={({isActive}) => isActive ? 'active' : ''}>
                         Lista Voucherów
                     </NavLink>
                 </li>
             </ul>
+            <label className="theme-switch">
+                <input
+                    type="checkbox"
+                    checked={darkMode}
+                    onChange={toggleDarkMode}
+                /> Dark Mode
+                <span className="slider"></span>
+            </label>
         </nav>
     );
 };
