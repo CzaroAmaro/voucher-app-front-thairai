483da8bbe74751b158ea0e3833292a58ce86c501
Cezary Babik <babik.czarek@gmail.com>
2025-02-25 12:42:25 +0100

    select

--- src/components/ReportGenerator/ReportGenerator.tsx
+++ src/components/ReportGenerator/ReportGenerator.tsx
@@ -28,13 +28,25 @@ const ReportGenerator: React.FC = () => {
             <form onSubmit={handleGenerateReport} className="report-form">
                 <div className="form-group">
                     <label>Miesiąc:</label>
-                    <input
-                        type="number"
+                    <select
                         value={month}
                         onChange={(e) => setMonth(e.target.value)}
-                        placeholder="Wpisz miesiąc (1-12)"
                         required
-                    />
+                    >
+                        <option value="">Wybierz miesiąc</option>
+                        <option value="1">Styczeń</option>
+                        <option value="2">Luty</option>
+                        <option value="3">Marzec</option>
+                        <option value="4">Kwiecień</option>
+                        <option value="5">Maj</option>
+                        <option value="6">Czerwiec</option>
+                        <option value="7">Lipiec</option>
+                        <option value="8">Sierpień</option>
+                        <option value="9">Wrzesień</option>
+                        <option value="10">Październik</option>
+                        <option value="11">Listopad</option>
+                        <option value="12">Grudzień</option>
+                    </select>
                 </div>
                 <div className="form-group">
                     <label>Rok:</label>
--- src/components/ReportListGenerator/ReportListGenerator.tsx
+++ src/components/ReportListGenerator/ReportListGenerator.tsx
@@ -65,13 +65,25 @@ const ReportListGenerator: React.FC = () => {
             <form onSubmit={handleFetchVouchers} className="fetch-form">
                 <div className="form-group">
                     <label>Miesiąc:</label>
-                    <input
-                        type="number"
+                    <select
                         value={month}
                         onChange={(e) => setMonth(e.target.value)}
-                        placeholder="Miesiąc (1-12)"
                         required
-                    />
+                    >
+                        <option value="">Wybierz miesiąc</option>
+                        <option value="1">Styczeń</option>
+                        <option value="2">Luty</option>
+                        <option value="3">Marzec</option>
+                        <option value="4">Kwiecień</option>
+                        <option value="5">Maj</option>
+                        <option value="6">Czerwiec</option>
+                        <option value="7">Lipiec</option>
+                        <option value="8">Sierpień</option>
+                        <option value="9">Wrzesień</option>
+                        <option value="10">Październik</option>
+                        <option value="11">Listopad</option>
+                        <option value="12">Grudzień</option>
+                    </select>
                 </div>
                 <div className="form-group">
                     <label>Rok:</label>
