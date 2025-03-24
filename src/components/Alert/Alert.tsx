592af137dd0476c902d6cd32ffb57caf274950d7
Cezary Babik <babik.czarek@gmail.com>
2025-03-24 12:09:53 +0100

    alert

---
+++ src/components/Alert/Alert.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Alert.css";
+
+interface AlertProps {
+    message: string;
+    onClose: () => void;
+}
+
+const Alert: React.FC<AlertProps> = ({ message, onClose }) => {
+    return (
+        <div className="alert-overlay">
+            <div className="alert-box">
+                <div className="alert-message">{message}</div>
+                <button className="alert-close-button" onClick={onClose}>
+                    OK
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Alert;
